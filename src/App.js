23dd52a6d450eb764f6c24b97e946d9c4b5a4049
janne-remes <janneremes@outlook.com>
2020-05-18 13:53:03 +0300

    React JS Crash Course -videon kohdassa 31:58.

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Lue maantiedon kokeisiin",
+        completed: false
+      },
+
+      {
+        id: 2,
+        title: "Käytä koiraa ulkona",
+        completed: false
+      },
+
+      {
+        id: 3,
+        title: "Mene sählytreeneihin",
+        completed: false
+      }
+    ]
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
+}
+
+export default App;
